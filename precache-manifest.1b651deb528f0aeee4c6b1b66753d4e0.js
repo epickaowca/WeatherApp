619f12a12ddbfe6543765d1db8985d2ce68596c5
Epicka Owca <owcaofficial@yahoo.com>
2020-03-27 16:45:26 +0100

    Updates

---
+++ precache-manifest.1b651deb528f0aeee4c6b1b66753d4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810f1fbc0973f1b49749e69eeaec9c88",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "7e763a07ab0cc63fd6f6",
+    "url": "/weatherApp/static/css/main.66f8e76b.chunk.css"
+  },
+  {
+    "revision": "410ad16b45c1a3e9433b",
+    "url": "/weatherApp/static/js/2.4537a064.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.4537a064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e763a07ab0cc63fd6f6",
+    "url": "/weatherApp/static/js/main.8e1a2f24.chunk.js"
+  },
+  {
+    "revision": "d808d008bb8f9c392b0f",
+    "url": "/weatherApp/static/js/runtime-main.9dc0af7a.js"
+  }
+]);
