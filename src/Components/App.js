4c2a01c720ec98f49d4dee8394c68c3d7b5014cb
Epicka Owca <owcaofficial@yahoo.com>
2020-03-27 17:47:17 +0100

    commit deploy

--- src/Components/App.js
+++ src/Components/App.js
@@ -26,7 +26,7 @@ class App extends Component{
 
   componentDidUpdate(prevProps, prevState){
     if(prevState.value !== this.state.value){
-      const API=`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}&units=metric`
+      const API=`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}&units=metric`
   
       fetch(API)
       .then(res => {
